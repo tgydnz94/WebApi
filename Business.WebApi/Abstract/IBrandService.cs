7c90409b0a02384f435565fbd0365359e8501db1
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-02-25 21:48:04 +0300

    business

---
+++ Business.WebApi/Abstract/IBrandService.cs
@@ -0,0 +1,18 @@
+﻿using Business.WebApi.Dtos.Requests;
+using Business.WebApi.Dtos.Responses;
+using Entities.WebApi.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.WebApi.Abstract
+{
+    public interface IBrandService
+    {
+        CreatedBrandResponse Add(CreateBrandRequest brand);
+        List<Brand> GetAll();
+    }
+    //responses and requests
+}
---
+++ Business.WebApi/Concrete/BrandManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.WebApi.Abstract;
+using Business.WebApi.Dtos.Requests;
+using Business.WebApi.Dtos.Responses;
+using DataAccess.WebApi.Abstract;
+using Entities.WebApi.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.WebApi.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+        private readonly IBrandDal _brandDal;
+
+        public BrandManager(IBrandDal brandDal )
+        {
+            _brandDal=brandDal;
+        }
+        public CreatedBrandResponse Add(CreateBrandRequest brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Brand> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
