7c14499c43b14c95f4b5c17b47947c22aa6133e3
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-02-25 21:36:46 +0300

    entity

---
+++ Entities.WebApi/Concrete/Brand.cs
@@ -0,0 +1,14 @@
+﻿using Core.WebApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.WebApi.Concrete
+{
+    public class Brand : BaseEntity<int>
+    {
+        public string Name { get; set; }
+    }
+}
