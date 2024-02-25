8f7433020d513d7aa285adf09cd7172899272ce2
Tugay <Tugay@DESKTOP-8EHVPE9>
2024-02-25 21:41:05 +0300

    dataaccess

---
+++ DataAccess.WebApi/Abstract/IBrandDal.cs
@@ -0,0 +1,15 @@
+﻿using Entities.WebApi.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.WebApi.Abstract
+{
+    public interface IBrandDal
+    {
+        void Add(Brand brand);
+        List<Brand> GetAll();
+    }
+}
